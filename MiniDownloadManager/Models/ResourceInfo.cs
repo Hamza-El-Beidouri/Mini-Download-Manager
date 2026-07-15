fe35963a150a19435ad030d297442ff4e6e57a40
Hamza El Beidouri <hamzaelbeidouri296@gmail.com>
2026-07-15 09:51:05 +0100

    refactor: derive size availability instead of storing IsSizeKnown

--- MiniDownloadManager/Models/ResourceInfo.cs
+++ MiniDownloadManager/Models/ResourceInfo.cs
@@ -5,7 +5,6 @@ public class ResourceInfo
     public string? SuggestedFileName  { get; }
     public string? ContentType { get; }
     public long? Size { get; }
-    public bool IsSizeKnown { get; }
     
     public ResourceInfo(string? suggestedFileName, string? contentType, long? size = null)
     {
@@ -13,7 +12,6 @@ public class ResourceInfo
         ContentType = contentType;
         Size = size;
         
-        IsSizeKnown = Size != null;
     }
     
 }
